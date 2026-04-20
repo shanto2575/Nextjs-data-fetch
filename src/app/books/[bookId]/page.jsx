e3ffa8c86d9d8e7f0aa71096fd5f5ac6abc6606a
shanto2575 <shanto2575@gmail.com>
2026-04-20 10:54:34 +0600

    s

--- src/app/books/[bookId]/page.jsx
+++ src/app/books/[bookId]/page.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+export const generateStaticParams=async()=>{
+    const res=await fetch('http://localhost:5000/books')
+    const book=await res.json()
+    
+    return book.slice(1,3).map(book=>({bookid:book.id}))
+}
 const BooksDetailsPage = async({params}) => {
     const {bookId}=await params;
 
